4727ea3663aec81c3eeb421381b346a280532c87
Damian <damian8100@gmail.com>
2022-05-04 19:01:53 +0200

    ProductController - wstpna implementacja endpointów

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,37 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/product")
 public class ProductController {
+
+    @GetMapping
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = {"productId"})
+    public ProductDto getProduct(@PathVariable long productId) {
+        return new ProductDto(1,"Produkt1",12,4);
+    }
+    @DeleteMapping
+    public void deleteProduct(Long productId) {
+    }
+
+    @PutMapping
+    public ProductDto updateProduct(ProductDto productDto){
+        return new ProductDto(1,"Produkt1.1",14,2);
+    }
+
+    @PostMapping
+    public void createProduct(ProductDto productDto){
+
+    }
+
 }
+
+
