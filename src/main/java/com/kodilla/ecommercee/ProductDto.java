64c2bcfdf5174e8fa573fe57c8947e75b2e2f29d
Damian <damian8100@gmail.com>
2022-05-06 18:21:18 +0200

    ProductController - wstpna implementacja endpointów

---
+++ src/main/java/com/kodilla/ecommercee/ProductDto.java
@@ -0,0 +1,14 @@
+package com.kodilla.ecommercee;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ProductDto {
+
+    private int productId;
+    private String name;
+    private double price;
+    private int stock;
+}
