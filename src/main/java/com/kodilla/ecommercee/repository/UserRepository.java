9a549e8e8bfbff528e6b357482422daa963f723e
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-05-09 22:23:08 +0200

    JDP220502-15: Encja User - implementacja - dodanie metod do UserRepository

--- src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
@@ -4,7 +4,21 @@ import com.kodilla.ecommercee.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Override
+    List<User> findAll();
+
+    @Override
+    Optional<User> findById(Long id);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUserName(String userName);
+
+    @Override
+    void deleteById(Long id);
 
 }
