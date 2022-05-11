0cced88310840549b6b32d01de0cc4ede1455760
Guralraz <87212802+Guralraz@users.noreply.github.com>
2022-05-11 16:34:19 +0200

    JDP220502-11: Encja Order

---
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -0,0 +1,41 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "ORDER")
+public class Order {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "Order_ID")
+    private int OrderId;
+
+    /*
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "Cart_ID")
+    @NotNull
+    @Column(name = "Cart_ID")
+    private Cart cart;
+
+    @NotNull
+    @Column(name = "Status")
+    private Status status;
+    */
+
+    @NotNull
+    @Column(name = "Date")
+    private LocalDate date = LocalDate.now();
+
+}
