80af4e55af6d2076b390bec777adf435fdd0e9e4
Kamil Kasjaniuk <multiprojekt.kk@gmail.com>
2022-05-11 14:01:20 +0200

    JDP220502-13 - Encja Group - implementacja

---
+++ src/main/java/com/kodilla/ecommercee/domain/ProductGroup.java
@@ -0,0 +1,34 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Product_Group")
+public class ProductGroup {
+
+    @Id
+    @Column(name = "Product_Group_Id")
+    private Long id;
+
+    @Column(name = "Name")
+    private String name;
+
+    // delete this comment after Product class implementation
+    /*@OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "group",
+            cascade = {CascadeType.ALL},
+            fetch = FetchType.LAZY
+    )
+    private List<Product> products = new ArrayList<>();*/
+}
