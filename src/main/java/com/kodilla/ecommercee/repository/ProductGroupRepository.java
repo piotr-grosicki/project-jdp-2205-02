48891b300c5c4e0ca6ba095305b62be07595f038
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-05-18 21:34:45 +0200

    JDP220502-18: Testy encji ProductGroup

--- src/main/java/com/kodilla/ecommercee/repository/ProductGroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductGroupRepository.java
@@ -5,7 +5,10 @@ import com.kodilla.ecommercee.domain.ProductGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductGroupRepository extends JpaRepository<ProductGroup, Long> {
 
+    Optional<ProductGroup> findByName(String name);
 }
