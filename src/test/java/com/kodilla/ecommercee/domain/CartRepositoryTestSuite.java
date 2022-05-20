cebbe0b0463d63e7d0211a89bda325d73ac4d8b5
Guralraz <87212802+Guralraz@users.noreply.github.com>
2022-05-20 20:12:00 +0200

    JDP220502-19 Encja Cart - usunita niepotrzebna klasa testowa z pakietu repository

--- src/test/java/com/kodilla/ecommercee/domain/CartRepositoryTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/CartRepositoryTestSuite.java
@@ -1,17 +1,10 @@
 package com.kodilla.ecommercee.domain;
 
 import com.kodilla.ecommercee.repository.CartRepository;
-import com.kodilla.ecommercee.repository.ProductRepository;
 import com.kodilla.ecommercee.repository.UserRepository;
-import org.junit.Before;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
